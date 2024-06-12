238c21cf286a537204baffd26c1b849e64f4f577
anastasija-mickoska <147696859+anastasija-mickoska@users.noreply.github.com>
2024-06-12 21:32:31 +0200

    Add files via upload

---
+++ Travel/home.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="style.css">
+        <style>
+            body {
+                background-image: url('landscape1.jpg');
+                background-size: cover;
+                background-repeat: no-repeat;
+            }
+            #homepage_statement {
+                width:100%;
+                height:300px;
+                margin-top:20%;
+            }
+            #home {
+                text-align: center;
+                position:relative;
+                width:100%;
+                font-size:64px;
+                letter-spacing: 0.25em;
+                font-family:'Poppins','sans-serif';
+                font-weight: 500;
+            }
+            #home2 {
+                text-align: center;
+                position:relative;
+                width:100%;
+                font-size:36px;
+                letter-spacing: 0.2em;
+                font-family:'Poppins','sans-serif';
+                font-weight: 300;
+            }
+        </style>
+    </head>
+    <body>
+        <div id="homepage_statement">
+            <p id="home">Let's travel the world!
+            </p>
+            <p id="home2">
+                Travel. Explore. Experience. 
+            </p>
+        </div>
+    </body>
+</html>
