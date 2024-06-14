235f63cc4f97d00458b76e33af9c8b8cdb2f16e9
anastasija-mickoska <147696859+anastasija-mickoska@users.noreply.github.com>
2024-06-14 15:43:17 +0200

    Add files via upload

---
+++ Travel/login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="style.css">
+        <style>
+        </style>
+    </head>
+    <body>
+            <nav>
+                <ul>
+                    <a href="home.php"><li>Home</li></a>
+                    <a href="summer.php"><li>Summer</li></a>
+                    <a href="winter.php"><li>Winter</li></a>
+                    <a href="spring_fall.php"><li>Spring and fall</li></a>
+                    <a href="login.php"><li>Login</li></a>
+                </ul>
+            </nav>
+    </body>
+</html>
