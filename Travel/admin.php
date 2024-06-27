7899b4bd94b166af6d703e211e09bf35ed7e8ba3
Anastasija Mickoska <147696859+anastasija-mickoska@users.noreply.github.com>
2024-06-27 14:32:43 +0200

    Update admin.php

--- Travel/admin.php
+++ Travel/admin.php
@@ -13,13 +13,16 @@ $arrangements=get_arrangements($conn);
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Admin</title>
     <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="style.php">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
     <style>
         body {
             background: linear-gradient(#085a57, rgba(8, 90, 87, 0.4)) no-repeat center center/cover;
             min-height: 100vh;
+        }
+        section {
+            width:100%;
+            float:left;
         }
         .categories {
             width:80%;
@@ -71,11 +74,9 @@ $arrangements=get_arrangements($conn);
         <nav>
             <ul>
                     <a href="home.php"><li>Home</li></a>
-                    <a href="summer.php"><li>Summer</li></a>
-                    <a href="winter.php"><li>Winter</li></a>
-                    <a href="spring.php"><li>Spring</li></a>
-                    <a href="fall.php"><li>Fall</li></a>
-                    <a href="login.php"><li>Login</li></a>
+                    <a href="#categories"><li>Categories</li></a>
+                    <a href="#allDestinations"><li>Arrangements</li></a>
+                    <a href="#allBookings"><li>Bookings</li></a>
                 </ul>
         </nav>
        <form action="search.php" method="get" style="width: 100%; max-width: 30rem">
@@ -84,8 +85,6 @@ $arrangements=get_arrangements($conn);
 		  <button class="input-group-text btn btn-primary">SEARCH</button>
 		</div>
        </form>
-
-       		<a class ="btn btn-danger" href="add_arrangement.php">Add arrangement</a>
        <div class="mt-5"></div>
         <?php if (isset($_GET['error'])) { ?>
           <div class="alert alert-danger" role="alert">
@@ -97,40 +96,47 @@ $arrangements=get_arrangements($conn);
 			  <?=htmlspecialchars($_GET['success']); ?>
 		  </div>
 		<?php } ?>
-
-		<h4>All Categories</h4>
-        <div class="categories">
-            <?php 
-            foreach ($categories as $category) {
-                $pageName = strtolower($category['categoryName']) . '.php';
-            ?>
-            <div class="card text-center">
-                <div class="card-body">
-                    <h5 class="card-title"><?php echo htmlspecialchars($category['categoryName']); ?></h5>
-                    <button class="btn btn-primary">
-                        <a href="<?php echo $pageName; ?>" class="adminButton">Details</a>
-                    </button>
-                    <button class="btn btn-primary">
-                        <a href="delete_category.php?id=<?php echo $category['categoryID']; ?>" class="adminButton">Delete all</a>
-                    </button>
-                </div>
-            </div>
-            <?php } ?>
-        </div>
-        <h4>All arrangements</h4>
-        <div class="arrangements">
-            <?php 
-            foreach($arrangements as $arrangement) { ?>
-                <a href="arrangement_details.php?id=<?php echo $arrangement['destinationID']?>">
-                    <div class="arr1">
-                        <div class="desc">
-                            <h3><?php echo $arrangement['destinationName']?></h3>
-                            <h4><i><?php echo $arrangement['fromDate']?> - <?php echo $arrangement['toDate']?></i></h4>
-                            <h4 class="price"><?php echo $arrangement['price']?></h4>
-                        </div>
+        <section id="categories">
+            <h4 id="allDestinations">All Categories</h4>
+            <div class="categories">
+                <?php 
+                foreach ($categories as $category) {
+                    $pageName = strtolower($category['categoryName']) . '.php';
+                ?>
+                <div class="card text-center">
+                    <div class="card-body">
+                        <h5 class="card-title"><?php echo htmlspecialchars($category['categoryName']); ?></h5>
+                        <button class="btn btn-primary">
+                            <a href="<?php echo $pageName; ?>" class="adminButton">Details</a>
+                        </button>
+                        <button class="btn btn-primary">
+                            <a href="delete_category.php?id=<?php echo $category['categoryID']; ?>" class="adminButton">Delete all</a>
+                        </button>
                     </div>
-                </a>
-            <?php } ?>
-        </div>
+                </div>
+                <?php } ?>
+            </div>
+        </section>
+        <section id="destinations">
+            <h4 id="allDestinations">All arrangements</h4>
+            <a class ="btn btn-danger" href="add_arrangement.php">Add arrangement</a>
+            <div class="arrangements">
+                <?php 
+                foreach($arrangements as $arrangement) { ?>
+                    <a href="arrangement_details.php?id=<?php echo $arrangement['destinationID']?>">
+                        <div class="arr1" style="background: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url('<?php echo $arrangement['imgUrl'];?>') no-repeat center center/cover;" >
+                            <div class="desc">
+                                <h3><?php echo $arrangement['destinationName']?></h3>
+                                <h4><i><?php echo $arrangement['fromDate']?> - <?php echo $arrangement['toDate']?></i></h4>
+                                <h4 class="price"><?php echo $arrangement['price']?></h4>
+                            </div>
+                        </div>
+                    </a>
+                <?php } ?>
+            </div>
+        </section>
+        <section id="bookings">
+            <h4 id="allBookings">All bookings</h4>
+        </section>
 </body>
 </html>
