0586db8b0b4b1e1076028b3156f3daa27caacadd
Anastasija Mickoska <147696859+anastasija-mickoska@users.noreply.github.com>
2024-06-30 02:08:08 +0200

    dodaden css

--- Travel/contact.php
+++ Travel/contact.php
@@ -1,3 +1,6 @@
+<?php 
+session_start();
+?>
 <!DOCTYPE html>
 <html>
 
@@ -10,88 +13,33 @@
             background: linear-gradient(#085a57, rgba(8, 90, 87, 0.4)) no-repeat center center/cover;
         }
 
-        .Login {
-            height: 100vh;
-        }
-
-        .login {
-            width: 40%;
-            margin-left: 30%;
-            margin-right: 30%;
-            box-shadow: 3px 3px 5px gray;
-            border-radius: 10px;
-            height: 60%;
-            margin-top: 5%;
-            margin-bottom: 5%;
-            background-color: white;
-            opacity: 0.5;
-            padding: 2%;
-        }
-
-        label {
-            font-family: 'Poppins', 'sans-serif';
-            font-weight: 300;
-            font-size: 1.5em;
-            letter-spacing: 0.2em;
-            margin-bottom: 10%;
-        }
-
-        input {
-            width: 45%;
-            padding: 2%;
-            border-radius: 5px;
-            background-color: white;
-            font-size: 1em;
-            font-family: 'Poppins', 'sans-serif';
-            font-weight: 300;
-            color: black;
-        }
-
         form {
             margin-top: 10%;
             width: 100%;
         }
-
-        .input1 {
+        .mb-4  {
             width: 100%;
-            margin: 5%;
-            margin-left: 25%;
-        }
-
-        #loginButton {
-            border-radius: 10px;
-            border: none;
-            width: 50%;
-            margin-left: 25%;
-            background-color: #085a57;
-            color: white;
-            padding: 2%;
-            font-size: 1.5em;
-            letter-spacing: 0.1em;
-        }
-
-        h1 {
             font-family: 'Poppins', 'sans-serif';
             font-weight: 300;
             font-size: 2.5em;
-            color: black;
-            letter-spacing: 0.1em;
-            text-align: center;
-            width: 100%;
-            margin-top: 2%;
-        }
-
-        h4 {
-            width: 50%;
-            font-family: 'Poppins', 'sans-serif';
-            font-weight: 300;
-            font-size: 1.5em;
-            color: black;
+            color: white;
             letter-spacing: 0.1em;
             text-align: center;
             margin-top: 2%;
+            text-decoration: underline 1px white;
         }
-
+        #btn {
+                border-radius:10px;
+                border:none;
+                width:50%;
+                margin-left:25%;
+                margin-top:5%;
+                background-color: #085a57;
+                color:white;
+                padding:2%;
+                font-size:1.5em;
+                letter-spacing: 0.1em;
+        }
         a {
             text-decoration: underline 1px black;
         }
@@ -128,18 +76,18 @@
             <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Us</h2>
             <form action="#" class="space-y-8">
                 <div>
-                    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
+                    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300" style="font-size:1.5em; color:white">Your email</label>
                     <input type="email" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required>
                 </div>
                 <div>
-                    <label for="subject" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Subject</label>
+                    <label for="subject" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300" style="font-size:1.5em; color:white ">Subject</label>
                     <input type="text" id="subject" class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Let us know how we can help you" required>
                 </div>
                 <div class="sm:col-span-2">
-                    <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
+                    <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400" style="font-size:1.5em; color:white">Your message</label>
                     <textarea id="message" rows="6" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Leave a comment..."></textarea>
                 </div>
-                <button type="submit" class="py-3 px-5 text-sm bg-black font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send message</button>
+                <button type="submit" id="btn" class="py-3 px-5 text-sm bg-black font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send message</button>
             </form>
         </div>
     </section>
